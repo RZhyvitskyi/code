0889175053df132f40be243e4f1717ba4eddf3dc
Rostyslav Zhyvitskyi <u0158236@karnovgroup.com>
2022-09-14 15:21:06 +0200

    show all added

--- rails_week_01/rails-task-manager/config/routes.rb
+++ rails_week_01/rails-task-manager/config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "tasks#index"
+  get "tasks", to: "tasks#index", as: "tasks"
+  get "tasks/new", to: "tasks#new", as: "new_task"
+  get "tasks/:id/edit", to: "tasks#edit", as: "edit_task"
+  get "tasks/:id", to: "tasks#show", as: "task"
+
+  post "tasks", to: "tasks#create"
+  patch "tasks/:id", to: "taks#update"
+  delete "tasks/:id", to: "tasks#destroy"
 end
