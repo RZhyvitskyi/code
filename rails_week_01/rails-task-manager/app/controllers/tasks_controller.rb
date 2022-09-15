04066992c2e8f776bf2a32c7ed6285f89f889629
Rostyslav Zhyvitskyi <u0158236@karnovgroup.com>
2022-09-15 10:20:06 +0200

    small styling

--- rails_week_01/rails-task-manager/app/controllers/tasks_controller.rb
+++ rails_week_01/rails-task-manager/app/controllers/tasks_controller.rb
@@ -14,7 +14,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
-    redirect_to task_path(@task)
+    redirect_to tasks_path(@task)
   end
 
   def edit
@@ -24,13 +24,13 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
     @task.update(task_params)
-    redirect_to root_path
+    redirect_to tasks_path
   end
 
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
-    redirect_to root_path, status: :see_other
+    redirect_to tasks_path, status: :see_other
   end
 
   private
--- rails_week_01/rails-task-manager/config/routes.rb
+++ rails_week_01/rails-task-manager/config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "tasks#index"
+  root to: "pages#home"
   get "tasks", to: "tasks#index", as: "tasks"
   get "tasks/new", to: "tasks#new", as: "new_task"
   get "tasks/:id/edit", to: "tasks#edit", as: "edit_task"
   get "tasks/:id", to: "tasks#show", as: "task"
+
+  get "/about", to: "pages#about", as: "about"
 
   post "tasks", to: "tasks#create"
   patch "tasks/:id", to: "tasks#update"
