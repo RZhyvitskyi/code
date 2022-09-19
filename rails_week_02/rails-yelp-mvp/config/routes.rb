e53344c75dad15fdada28441aab41b008b2b9e01
Rostyslav Zhyvitskyi <u0158236@karnovgroup.com>
2022-09-19 15:57:34 +0200

    styling coppied from the last task

--- rails_week_02/rails-yelp-mvp/config/routes.rb
+++ rails_week_02/rails-yelp-mvp/config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root to: 'pages#home'
+
+  get "/home", to: "pages#home", as: "home"
+  get "/about", to: "pages#about", as: "about"
 end
