dff6e61bb95eb4276dea16fe0ad0a0a6effe2ff7
Rostyslav Zhyvitskyi <u0158236@karnovgroup.com>
2022-09-19 14:42:30 +0200

    reviews work

--- rails_week_02/rails-yelp-mvp/config/routes.rb
+++ rails_week_02/rails-yelp-mvp/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, shallow: true, only: %i[new create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
