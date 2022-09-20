40f145de373833d3c572cde4b5046e16f9fabe1f
Rostyslav Zhyvitskyi <u0158236@karnovgroup.com>
2022-09-20 09:55:56 +0200

    more styling

--- rails_week_02/rails-yelp-mvp/app/models/review.rb
+++ rails_week_02/rails-yelp-mvp/app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :content, presence: true
-  validates :rating, inclusion: { in: (0..5) }
+  validates :rating, inclusion: { in: (0..5) }, presence: true
 end
