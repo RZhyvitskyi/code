ac30a5d426646cd82037ae90b8d3fb0346464cae
Rostyslav Zhyvitskyi <u0158236@karnovgroup.com>
2022-09-21 13:39:43 +0200

    devise views

--- rails_week_02/movie-watch-list/config/routes.rb
+++ rails_week_02/movie-watch-list/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+  end
+  
+  resources :lists, only: %i[index show create update destroy]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
