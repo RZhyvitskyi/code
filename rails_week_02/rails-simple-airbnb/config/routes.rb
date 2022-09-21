7cc44966028ef34de69963eb7e379c3f37e35fef
Rostyslav Zhyvitskyi <u0158236@karnovgroup.com>
2022-09-21 07:44:56 +0200

    header and footer added

--- rails_week_02/rails-simple-airbnb/config/routes.rb
+++ rails_week_02/rails-simple-airbnb/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+  get "/home", to: "pages#home", as: "home"
+  get "/about", to: "pages#about", as: "about"
+
   resources :flats do
     resources :reviews, shallow: true, only: %i[create update delete]
   end
