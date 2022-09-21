03e75857d412a5f55db3d18152d0ed79656be21e
Rostyslav Zhyvitskyi <u0158236@karnovgroup.com>
2022-09-21 12:34:36 +0200

    devise and db created

--- rails_week_02/movie-watch-list/config/environments/development.rb
+++ rails_week_02/movie-watch-list/config/environments/development.rb
@@ -70,4 +70,5 @@ Rails.application.configure do
 
   config.middleware.insert_after ActionDispatch::Static, Rack::LiveReload
   
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
