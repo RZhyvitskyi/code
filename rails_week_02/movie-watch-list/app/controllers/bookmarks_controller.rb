3d121004298d6ae3dd8daa11865d24aaffc9c67f
Rostyslav Zhyvitskyi <u0158236@karnovgroup.com>
2022-09-22 08:21:06 +0200

    create bookmark done

--- rails_week_02/movie-watch-list/app/controllers/bookmarks_controller.rb
+++ rails_week_02/movie-watch-list/app/controllers/bookmarks_controller.rb
@@ -1,4 +1,40 @@
 class BookmarksController < ApplicationController
+  before_action :get_movies, only: [:new]
+
   def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new()
+    @movie = Movie.find(bookmark_params[:movie_id])
+    @bookmark.movie = @movie
+    @bookmark.list = @list
+    @bookmark.comment = bookmark_params[:comment]
+    if @bookmark.save
+      redirect_to new_list_bookmark_path(@list), notice: 'List was successfully created.'
+    else
+      redirect_to new_list_bookmark_path(@list)
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :comment)
+  end
+
+  def get_movies
+    @movies = Movie.all
+  end
+
+  def get_user
+    @user = current_user
+  end
+
+  def get_list
+    @list = List.find(params[:id])
   end
 end
--- rails_week_02/movie-watch-list/config/routes.rb
+++ rails_week_02/movie-watch-list/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'bookmarks/new'
   devise_for :users do
   end
   
-  resources :lists
+  resources :lists do
+    resources :bookmarks, shallow: true, only: [:new, :create, :destroy]
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
